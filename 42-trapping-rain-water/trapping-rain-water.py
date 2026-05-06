148278b8ce09014d627b2a68dbe71f6a6ae2b3db
AllenHe <theamazing.appletree@gmail.com>
2026-05-05 21:49:11 -0400

    Time: 3 ms (95.26%) | Memory: 21.1 MB (49.75%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,25 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        l,lx, r, rx = 0, 0, n-1, n-1
-        area = min(height[l],height[r]) * max(r-l-1, 0)
+        l, r = 0, len(height)-1
+        maxL, maxR = height[l], height[r]
+        area = 0
         while l < r:
-            if height[l] > height[lx]:
-                area -= height[lx]
-                area += (r-l-1) * (min(height[l],height[rx])-height[lx])
-                lx = l
-            elif height[r] > height[rx]:
-                area -= height[rx]
-                area += (r-l-1) * (min(height[lx],height[r])-height[rx])
-                rx = r
-            if height[lx] > height[rx]:
-                if r != rx:
-                    area -= height[r]
-                r -= 1
-            else:
-                if l != lx:
-                    area -= height[l]
+            if maxL < maxR:
+                area += maxL - height[l]
                 l += 1
+                maxL = max(maxL, height[l])
+            else:
+                area += maxR - height[r]
+                r -= 1
+                maxR = max(maxR, height[r])
         return area
 
         
