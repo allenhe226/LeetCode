caf180bed8d8031a477998ba28e2a98726df0f38
AllenHe <theamazing.appletree@gmail.com>
2026-05-06 00:27:21 -0400

    Time: 135 ms (93.43%) | Memory: 39.5 MB (95.81%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,21 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         n = len(position)
-        time = []
-        for i in range(n):
-            time.append((target-position[i])/speed[i])
-
-        posTime = [[position[i], time[i]] for i in range(n)]
-        posTime = sorted(posTime, key=lambda x:-x[0])
-        time = [posTime[i][1] for i in range(n)]
-
-        count = 0
         stack = []
-        for t in time:
-            while stack and t > stack[-1]:
-                stack.pop()
-            if not stack:
-                count += 1
-            stack.append(t)
-        return count
+        ps = [(p,s) for p,s in zip(position,speed)]
+        for p, s in sorted(ps)[::-1]:
+            t = (target-p)/s
+            while not stack or t > stack[-1]:
+                stack.append(t)
+        return len(stack)
         
