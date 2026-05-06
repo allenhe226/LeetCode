98567c06bf2a99a86eccc0f2b5fb1f5eefa3367f
AllenHe <theamazing.appletree@gmail.com>
2026-05-05 22:16:20 -0400

    Time: 71 ms (95.13%) | Memory: 56.5 MB (64.30%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -2,18 +2,14 @@ class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         m, n = len(boxGrid), len(boxGrid[0])
         grid = [["."] * m for _ in range(n)]
-        
-        for r in range(n):
-            for c in range(m):
-                grid[r][c] = boxGrid[m-c-1][r]
 
-        print(grid)
-        for r in range(n-1,-1,-1):
-            for c in range(m):
-                if grid[r][c] == "#":
-                    grid[r][c] = "."
-                    i = 1
-                    while r+i<n and grid[r+i][c] == ".":
-                        i += 1
-                    grid[r+i-1][c] = "#"
+        for r in range(m):
+            x = n-1
+            for c in range(n-1,-1,-1):
+                if boxGrid[r][c] == "*":
+                    grid[c][m-r-1] = "*"
+                    x = c-1
+                elif boxGrid[r][c] == "#":
+                    grid[x][m-r-1] = "#"
+                    x = x-1
         return grid
