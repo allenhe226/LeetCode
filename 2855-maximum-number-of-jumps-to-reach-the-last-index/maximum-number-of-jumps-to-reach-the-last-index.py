f6821a0f7e05d218d34d03397a6d913d795e9c6f
AllenHe <theamazing.appletree@gmail.com>
2026-05-09 23:59:04 -0400

    Time: 971 ms (5.23%) | Memory: 19.4 MB (64.71%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.py
@@ -1,18 +1,17 @@
 class Solution:
     def maximumJumps(self, nums: List[int], target: int) -> int:
-        from typing import List
-
-
-class Solution:
-    def maximumJumps(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        dp = [float("-inf")] * n
+        dp = [float("inf")] * n
         dp[0] = 0
-
-        for i in range(1, n):
+        for i in range(1,n):
             for j in range(i):
-                if abs(nums[j] - nums[i]) <= target:
-                    dp[i] = max(dp[i], dp[j] + 1)
+                if dp[j] != float("inf") and abs(nums[i]-nums[j]) <= target:
+                    if dp[i] == float("inf"):
+                        dp[i] = dp[j] + 1
+                    else:
+                        dp[i] = max(dp[i], dp[j]+1)
+        if dp[-1] == float("inf"):
+            return -1
+        return dp[-1]
 
-        return -1 if dp[n - 1] < 0 else dp[n - 1]
         
