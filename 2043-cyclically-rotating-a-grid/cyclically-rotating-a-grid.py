91a3cea7d9af2fe591603bfe796527ed9a643ec9
AllenHe <theamazing.appletree@gmail.com>
2026-05-09 20:07:56 -0400

    Time: 429 ms (5.48%) | Memory: 19.6 MB (67.12%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
@@ -1,34 +1,19 @@
 class Solution:
     def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m, n = len(grid), len(grid[0])
-        nlayer = min(m // 2, n // 2)  # level count
-        # enumerate each layer counterclockwise starting from the top-left corner
-        for layer in range(nlayer):
-            r = []  # row index of each element
-            c = []  # column index of each element
-            val = []  # value of each element
-            for i in range(layer, m - layer - 1):  # left
-                r.append(i)
-                c.append(layer)
-                val.append(grid[i][layer])
-            for j in range(layer, n - layer - 1):  # down
-                r.append(m - layer - 1)
-                c.append(j)
-                val.append(grid[m - layer - 1][j])
-            for i in range(m - layer - 1, layer, -1):  # right
-                r.append(i)
-                c.append(n - layer - 1)
-                val.append(grid[i][n - layer - 1])
-            for j in range(n - layer - 1, layer, -1):  # up
-                r.append(layer)
-                c.append(j)
-                val.append(grid[layer][j])
-            total = len(val)  # total number of elements in each layer
-            kk = k % total  # equivalent number of rotations
-            # find the value at each index after rotation
-            for i in range(total):
-                idx = (
-                    i + total - kk
-                ) % total  # the index corresponding to the value after rotation
-                grid[r[i]][c[i]] = val[idx]
+        dirs = [(1,0),(0,1),(-1,0),(0,-1)]
+        for i in range(min(m//2,n//2)):
+            l = 2*(m-2*i+n-2*i)-4
+            for j in range(k%l):
+                corners = {(i,i),(m-1-i,i),(m-1-i,n-1-i),(i,n-1-i)}
+                x, y, d = i, i, 0
+                first = grid[y][x]
+                while d < 4:
+                    dx, dy = dirs[d]
+                    nx, ny = x+dx, y+dy
+                    grid[y][x] = grid[ny][nx]
+                    if (ny,nx) in corners:
+                        d += 1
+                    y, x = ny, nx
+                grid[y+1][x] = first
         return grid
