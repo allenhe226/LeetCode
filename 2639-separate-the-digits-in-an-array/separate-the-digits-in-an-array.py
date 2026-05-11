3e15d1039b50e88945703ea4f3b833d1804de9bf
AllenHe <theamazing.appletree@gmail.com>
2026-05-10 20:11:03 -0400

    Time: 3 ms (76.03%) | Memory: 19.2 MB (98.35%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         ans = []
-        for i in range(len(nums)):
-            if nums[i] > 9:
-                ans += list(map(int, list(str(nums[i]))))
+        for num in nums:
+            if num > 9:
+                for digit in str(num):
+                    ans.append(int(digit))
             else:
-                ans.append(nums[i])
+                ans.append(num)
         return ans
         
