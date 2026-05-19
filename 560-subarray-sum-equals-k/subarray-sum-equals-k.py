07eb3fa42ce81b902c89dda86b34afc61bb7748b
AllenHe <theamazing.appletree@gmail.com>
2026-05-19 18:04:15 -0400

    Time: 32 ms (65.17%) | Memory: 21.9 MB (58.02%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -3,11 +3,11 @@ class Solution:
         n = len(nums)
         pre = 0
         count = 0
-        seen = defaultdict(int)
+        seen = {}
         seen[0] = 1
         for i in range(n):
             pre += nums[i]
             if pre - k in seen:
                 count += seen[pre-k]
-            seen[pre] += 1
+            seen[pre] = 1 + seen.get(pre,0)
         return count
