7e3fe9c4b4031daf0d0113225ba5c841eff626f0
AllenHe <theamazing.appletree@gmail.com>
2026-05-19 18:03:17 -0400

    Time: 47 ms (12.52%) | Memory: 21.9 MB (37.15%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,14 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        psa = [0]
-        for i in range(n):
-            psa.append(psa[-1] + nums[i])
-
+        pre = 0
         count = 0
         seen = defaultdict(int)
-        for i in range(n+1):
-            if psa[i] - k in seen:
-                count += seen[psa[i]-k]
-            seen[psa[i]] += 1
+        seen[0] = 1
+        for i in range(n):
+            pre += nums[i]
+            if pre - k in seen:
+                count += seen[pre-k]
+            seen[pre] += 1
         return count
