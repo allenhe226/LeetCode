b94fba2d455027a619a6be9e7f7f1c00ccae85c2
AllenHe <theamazing.appletree@gmail.com>
2026-05-24 18:59:47 -0400

    Time: 233 ms (42.36%) | Memory: 25 MB (27.67%) - LeetSync

--- 1466-jump-game-v/jump-game-v.py
+++ 1466-jump-game-v/jump-game-v.py
@@ -1,27 +1,17 @@
 class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         n = len(arr)
-        vals = {}
-
-        def findVal(i):
-            if i in vals:
-                return vals[i]
-            choices = [1]
+        @functools.cache
+        def getMaxJumps(i):
+            result = 0
             for k in range(1,d+1):
                 if i-k < 0 or arr[i-k] >= arr[i]:
                     break
-                choices.append(1+findVal(i-k))
+                result = max(result, 1+getMaxJumps(i-k))
             for k in range(1,d+1):
                 if i+k >= n or arr[i+k] >= arr[i]:
                     break
-                choices.append(1+findVal(i+k))
-            vals[i] = max(choices)
-            return max(choices)
-
-
-        maxval = 1
-        for i in range(n):
-            val = findVal(i)
-            maxval = max(maxval,val)
-        return maxval
+                result = max(result, 1+getMaxJumps(i+k))
+            return result
+        return 1+max(getMaxJumps(i) for i in range(n))
         
