795a06ea018b2dbd2b49e0bcf5170befd8af2564
AllenHe <theamazing.appletree@gmail.com>
2026-05-25 00:12:09 -0400

    Time: 105 ms (93.54%) | Memory: 23.8 MB (43.29%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.py
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -1,14 +1,16 @@
 class Solution:
     def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if s[-1] == "1":
+            return False
         n = len(s)
         rightMost = 0
         idxs = collections.deque([0])
         while idxs:
             i = idxs.popleft()
-            if i == n-1:
-                return True
-            for j in range(max(rightMost, i+minJump), min(i+maxJump+1, n)):
+            for j in range(max(rightMost, i+minJump), min(i+maxJump, n-1)+1):
                 if s[j] == "0":
+                    if j == n-1:
+                        return True
                     idxs.append(j)
             rightMost = i+maxJump+1
         return False
