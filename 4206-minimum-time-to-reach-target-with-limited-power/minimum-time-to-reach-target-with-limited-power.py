47c8ae401db1f8e4842ce1b8a06924232b30300f
AllenHe <theamazing.appletree@gmail.com>
2026-06-28 01:34:09 -0400

    Time: 606 ms (61.70%) | Memory: 38 MB (57.14%) - LeetSync

---
+++ 4206-minimum-time-to-reach-target-with-limited-power/minimum-time-to-reach-target-with-limited-power.py
@@ -0,0 +1,28 @@
+import heapq
+class Solution:
+    def minTimeMaxPower(self, n: int, edges: List[List[int]], power: int, cost: List[int], source: int, target: int) -> List[int]:
+        adj = [[] for _ in range(n)]
+        for u, v, t in edges:
+            adj[u].append((v, t))
+        q = [(0, -power, source)]
+        dist = [[float("inf")] * (power+1) for i in range(n)]
+        dist[source][power] = 0
+        while q:
+            t, p, node = heapq.heappop(q)
+            p = -p
+  
+            if node == target:
+                return [t, p]
+
+            if p < cost[node]:
+                continue
+            p -= cost[node]
+            
+            for neighbor, time in adj[node]:
+                if t+time < dist[neighbor][p]:
+                    dist[neighbor][p] = t + time
+                    heapq.heappush(q, (t+time, -p, neighbor))
+                    
+        return [-1,-1]
+            
+        
