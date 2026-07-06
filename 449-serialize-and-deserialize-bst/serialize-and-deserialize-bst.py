5b283334be117021b31a9257a9bebf64e61dbd85
AllenHe <theamazing.appletree@gmail.com>
2026-07-06 01:03:01 -0400

    Time: 75 ms (17.95%) | Memory: 23.2 MB (10.67%) - LeetSync

---
+++ 449-serialize-and-deserialize-bst/serialize-and-deserialize-bst.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+            return
+        dfs(root)
+        return " ".join(res)
+        
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        res = list(data.split(" "))
+        idx = -1
+        def dfs():
+            nonlocal idx
+            idx += 1
+            if res[idx] == "N":
+                return None
+            node = TreeNode(int(res[idx]))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+        
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
