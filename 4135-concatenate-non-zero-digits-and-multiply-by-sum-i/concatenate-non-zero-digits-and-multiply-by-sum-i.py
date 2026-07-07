7e94ae65aab553a1c0c36fdee33c1a446a4f6273
AllenHe <theamazing.appletree@gmail.com>
2026-07-07 02:27:51 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (14.68%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,12 +1,10 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        total = 0
-        new = 0
-        power = 0
+        s = p = n2 = 0
         while n:
             if n % 10 > 0:
-                new += n % 10 * pow(10, power)
-                power += 1
-                total += n % 10
+                n2 += n % 10 * pow(10, p)
+                p += 1
+                s += n % 10
             n //= 10
-        return new * total
+        return n2 * s
