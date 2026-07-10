c2cc4fa57ad8b14904dfc2d0416957556e70cb9d
AllenHe <theamazing.appletree@gmail.com>
2026-07-10 03:00:35 -0400

    Time: 93 ms (58.27%) | Memory: 20.8 MB (65.13%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -5,18 +5,21 @@ class Solution:
         for i in range(m):
             letters[tasks[i]] += 1
 
-        time = 0
+        time = 1
         maxHeap = []
         queue = collections.deque([])
         for key in letters:
             heapq.heappush(maxHeap, -letters[key])
         
         while maxHeap or queue:
-            time += 1
-            while queue and queue[0][1] <= time:
-                heapq.heappush(maxHeap, queue.popleft()[0])
             if maxHeap:
                 count = heapq.heappop(maxHeap)
                 if count + 1 < 0:
                     queue.append((count+1, time+n+1))
-        return time
+                time += 1
+                if queue and queue[0][1] <= time:
+                    heapq.heappush(maxHeap, queue.popleft()[0])
+            else:
+                time = queue[0][1]
+                heapq.heappush(maxHeap, queue.popleft()[0])
+        return time-1
