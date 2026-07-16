f90c098528817d06955800ac6215a4860097d14d
AllenHe <theamazing.appletree@gmail.com>
2026-07-15 23:16:59 -0400

    Time: 219 ms (41.32%) | Memory: 33.7 MB (55.09%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.py
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.py
@@ -1,21 +1,15 @@
+import math
 class Solution:
     def gcdSum(self, nums: list[int]) -> int:
-        def gcd(x,y):
-            if y == 0:
-                return x
-            if y > x:
-                return gcd(y,x)
-            return gcd(y, x%y)
-        
         n = len(nums)
         mx = nums[0]
         prefixGcd = [0] * n
         for i in range(n):
             mx = max(mx, nums[i])
-            prefixGcd[i] = gcd(mx, nums[i])
+            prefixGcd[i] = math.gcd(mx, nums[i])
         prefixGcd.sort()
 
         total = 0
         for i in range(n//2):
-            total += gcd(prefixGcd[i], prefixGcd[n-i-1])
+            total += math.gcd(prefixGcd[i], prefixGcd[n-i-1])
         return total
