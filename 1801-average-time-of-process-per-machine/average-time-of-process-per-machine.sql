6d5f76fc862a2bbefc748f8ba8bb49d3bfe6c2af
AllenHe <theamazing.appletree@gmail.com>
2026-07-17 20:25:44 -0400

    Time: 244 ms (84.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,4 @@
+SELECT a1.machine_id, ROUND((SUM(a2.timestamp) - SUM(a1.timestamp)) / COUNT(a2.timestamp) * 1000)/1000 AS processing_time
+FROM Activity a1, Activity a2
+WHERE a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id AND a1.activity_type = "start" AND a2.activity_type = "end"
+GROUP BY a1.machine_id;
