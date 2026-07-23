78c545cf444b6d5c1459a7faee31fc290285e2b4
AllenHe <theamazing.appletree@gmail.com>
2026-07-23 02:07:32 -0400

    Time: 0 ms (100.00%) | Memory: 33.5 MB (45.76%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
@@ -1,8 +1,3 @@
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n < 3:
-            return n
-        if n == 3:
-            return 4
-        return pow(2, n.bit_length())
+        return pow(2, len(nums).bit_length() - 1 * (len(nums) <= 2))
