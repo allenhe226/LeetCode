d3e198a8b0b01bc35a82e1435c2f9d1c0593c8f8
AllenHe <theamazing.appletree@gmail.com>
2026-07-25 01:11:07 -0400

    Time: 404 ms (28.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,3 @@
+SELECT query_name, ROUND(SUM(rating/position)/COUNT(query_name), 2) AS quality, ROUND(SUM(CASE WHEN rating < 3 THEN 100 ELSE 0 END) / COUNT(query_name), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
