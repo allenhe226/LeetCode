8a3a2aff91963a448808489d33acbcd43fa95860
AllenHe <theamazing.appletree@gmail.com>
2026-08-03 18:48:47 -0400

    Time: 248 ms (59.80%) | Memory: 51.7 MB (48.38%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -3,21 +3,5 @@ class Solution:
         possible = set()
         for i in range(len(s)-k+1):
             possible.add(s[i:i+k])
-        self.res = True
-        self.cur = []
-        def backtrack():
-            if not self.res:
-                return
-            if len(self.cur) == k:
-                if "".join(self.cur) not in possible:
-                    self.res = False
-                return
-            self.cur.append("0")
-            backtrack()
-            self.cur.pop()
-            self.cur.append("1")
-            backtrack()
-            self.cur.pop()
-        backtrack()
-        return self.res
+        return len(possible) == pow(2, k)
             
